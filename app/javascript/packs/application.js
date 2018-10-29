ba4c669a64003fd216ab152945e8fab228316b12
Harry Johnson <hj-mac@dhcp-wifi-8021x-155-41-80-90.bu.edu>
2018-10-29 12:07:25 -0400

    application.js changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,5 +6,25 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import React from 'react'
+import { render } from 'react-dom'
 
-console.log('Hello World from Webpacker')
+import App from '../react/components/app'
+import RedBox from 'redbox-react'
+
+document.addEventListener('DOMContentLoaded', () => {
+  let reactElement = document.getElementById('app')
+
+  if (reactElement) {
+    if(window.railsEnv && window.railsEnv === 'development'){
+      try {
+        render(<App />, reactElement)
+      } catch (e) {
+        render(<RedBox error={e} />, reactElement)
+      }
+    }
+    else {
+      render(<App />, reactElement)
+    }
+  }
+}) 
