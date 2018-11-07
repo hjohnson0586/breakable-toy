cbee1f913a311cf976d43ae19ddb9b9201592dc8
Harry Johnson <hj-mac@Harrys-MBP.fios-router.home>
2018-11-07 14:25:42 -0500

    First css commit

--- app/javascript/react/components/BuildTile.js
+++ app/javascript/react/components/BuildTile.js
@@ -3,17 +3,17 @@ import React from 'react'
 const BuildTile = props => {
   let buildIndexHTML = props.builds.map(build => {
     return(
-      <div key={build.id} >
+      <div key={build.id}>
         <div>
-          <h3><a href={`/builds/${build.id}`}>{build.name}</a></h3>
-            <p>{build.description}</p>
-            <p>${build.price}</p>
+          <h3><a className="build-title" href={`/builds/${build.id}`}>{build.name}</a></h3>
+            <p className="build-description">{build.description}</p>
+            <p className="build-price">${build.price}</p>
         </div>
       </div>
     )
   })
   return(
-    <div>
+    <div className="build-tile-css">
       {buildIndexHTML}
     </div>
   )
